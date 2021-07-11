6ceb93ddc2f3cbe6db3fa18b29b10c129c5d01d1
tnmy2426 <al.nahdi.tnmy@gmail.com>
2021-07-11 05:24:53 -0700

    peofile_page & change_profile_page design complete

--- App_Login/forms.py
+++ App_Login/forms.py
@@ -6,13 +6,17 @@ from App_Login.models import UserProfile
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(label="Email Address", required=True)
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+
 class UserProfileChange(UserChangeForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'password')
+
 
 class ProfilePic(forms.ModelForm):
     class Meta:
