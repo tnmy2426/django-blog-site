836351d0e1e02129aa86b8774f558d341dde649d
tnmy2426 <al.nahdi.tnmy@gmail.com>
2021-08-25 22:55:20 -0700

    Default SQlite database added

--- My_Blog_Project/settings.py
+++ My_Blog_Project/settings.py
@@ -76,11 +76,11 @@ WSGI_APPLICATION = 'My_Blog_Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-# 'ENGINE': 'django.db.backends.sqlite3',
-# 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
