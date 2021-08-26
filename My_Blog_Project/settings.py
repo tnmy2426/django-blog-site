458877e20bdc509ddc895728fde5de2b70cf52f5
tnmy2426 <al.nahdi.tnmy@gmail.com>
2021-08-25 22:51:19 -0700

    Database problem solved

--- My_Blog_Project/settings.py
+++ My_Blog_Project/settings.py
@@ -74,12 +74,13 @@ WSGI_APPLICATION = 'My_Blog_Project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        
+    }
+}
+# 'ENGINE': 'django.db.backends.sqlite3',
+# 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
